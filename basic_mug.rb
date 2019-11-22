e279fb6217d6abca1d7ff5ba3b6e54b9800041c6
Polly <pollymarthasmith@gmail.com>
2019-11-22 14:50:18 +0000

    First commit

---
+++ basic_mug.rb
@@ -0,0 +1,13 @@
+class BasicMug
+  def fill_with(beverage: :coffee)
+    @beverage = beverage
+    puts "Your mug is filled with #{beverage}"
+  end
+
+  def use
+    puts "What is your beverage of choice?"
+    answer = gets.chomp
+    fill_with(beverage: answer)
+    "Enjoy your '#{@beverage} on Day of Code!"
+  end
+end
